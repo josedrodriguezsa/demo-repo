787fe4aba60742cf693ac76d8beecd0fb62df445
Jose Rodriguez <jr@gmail.com>
2024-07-25 17:37:59 +0200

    Commit changes test

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Some description!
 
 ## Subheader
 
-Watch tutorial
+Watch tutorialc
---
+++ buggy.py
@@ -0,0 +1,8 @@
+def buggy_function(x):
+    y = x * 2
+    z = y + 10
+    return z
+
+# Call the function
+result = buggy_function(5)
+print(result)
